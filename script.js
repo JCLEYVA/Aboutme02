14cd340f637bb7558f2ad756a11ac4c0db59f865
JEANETTE LEYVA <jeanettecleyva@outlook.com>
2023-03-07 19:18:00 -0700

    finished day 2

---
+++ script.js
@@ -0,0 +1,27 @@
+let yourName = prompt("What's up, what do I call you?");
+console.log(yourName);
+alert("That's a dope name, " + yourName + ". Do you want to play a guessing game? ");
+
+let firstHobby = prompt("Is painting my preffered art medium? y/n");
+console.log(firstHobby);
+alert("I prefer drawing! Ive been drawing since I was five.");
+
+let secondHobby = prompt("Is baseball my favorite sport? y/n");
+console.log(secondHobby);
+alert("Baseball is the best sport, Go Dodgers !");
+
+let thirdhobby = prompt("Do I like to stay on google all day? y/n");
+console.log(thirdhobby);
+alert("Every piece of information is useful to someone, I stay on google researching topics for conversation starters");
+
+let fourthhobby = prompt("Is Salmon my favorite seafood? y/n");
+console.log(fourthhobby);
+alert("I eat my weight in shrimp, it's my favorite");
+
+let fifthhobby = prompt("Is my sexual identity important to me? y/n?");
+console.log(fifthhobby);
+alert("My sexuality used to be important to me. It no longer is; the older I get");
+
+alert("Thanks for stopping by" + yourName);
+
+
